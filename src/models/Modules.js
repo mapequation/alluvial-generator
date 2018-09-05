3d16d845d48512bb4983581801b5a7af6b3b4451
Anton Eriksson <anton.eriksson@umu.se>
2018-09-05 16:40:13 +0200

    Modules with near zero height should not be added.

--- src/models/Modules.js
+++ src/models/Modules.js
@@ -20,23 +20,30 @@ export default class Modules {
         return this._modulesWithHeightY(this.modules, this.maxTotalFlow, height, padding);
     }
 
-    _modulesWithHeightY(modules, totalFlow, totalHeight, padding) {
-        const totalPadding = padding * (modules.length - 1);
-        const usableHeight = totalHeight - totalPadding;
-
+    _modulesWithHeightY(modules, totalFlow, totalHeight, padding, minHeight = 0.1) {
         let accumulatedHeight = totalHeight; // starting from the bottom, so we subtract from this
 
-        return modules.map(module => {
-            const height = module.flow / totalFlow * usableHeight;
-            const y = accumulatedHeight - height;
-            accumulatedHeight -= height + padding;
-            return {
-                width: this.style.barWidth,
-                height,
-                x: this.xOffset,
-                y,
-                ...module,
-            };
-        });
+        const visibleModules = modules.filter(module =>
+            this._moduleHeight(module.flow, padding, modules.length, totalHeight, totalFlow) > minHeight);
+
+        return visibleModules
+            .map(module => {
+                const height = this._moduleHeight(module.flow, padding, visibleModules.length, totalHeight, totalFlow);
+                const y = accumulatedHeight - height;
+                accumulatedHeight -= height + padding;
+                return {
+                    width: this.style.barWidth,
+                    height,
+                    x: this.xOffset,
+                    y,
+                    ...module,
+                };
+            });
+    }
+
+    _moduleHeight(flow, padding, numModules, totalHeight, totalFlow) {
+        const totalPadding = padding * (numModules - 1);
+        const usableHeight = totalHeight - totalPadding;
+        return flow / totalFlow * usableHeight;
     }
 }
