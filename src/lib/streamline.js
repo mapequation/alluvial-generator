073357852868cfa2f092a93a1a937d0fff34599d
Anton Eriksson <anton.eriksson@umu.se>
2018-09-18 16:51:36 +0200

    Use d3 path instead of linkHorizontal

--- src/lib/streamline.js
+++ src/lib/streamline.js
@@ -1,10 +1,17 @@
-import { linkHorizontal } from "d3";
+// @flow
+import { path } from "d3";
 
-export const streamlineHorizontal = () => {
-    const link = linkHorizontal();
-    return function streamline([p1, p2, p3, p4]) {
-        return `${link({ source: p1, target: p2 })}L${p3}${link({ source: p3, target: p4 })}L${p1}`;
+
+export type Point = [number, number];
+
+export function streamlineHorizontal() {
+    return function streamline([p0, p1, p2, p3]: [Point, Point, Point, Point]): string {
+        const context = path();
+        context.moveTo(p0[0], p0[1]);
+        context.bezierCurveTo((p0[0] + p1[0]) / 2, p0[1], (p0[0] + p1[0]) / 2, p1[1], p1[0], p1[1]);
+        context.lineTo(p2[0], p2[1]);
+        context.bezierCurveTo((p2[0] + p3[0]) / 2, p2[1], (p2[0] + p3[0]) / 2, p3[1], p3[0], p3[1]);
+        context.closePath();
+        return context.toString();
     };
-};
-
-
+}
