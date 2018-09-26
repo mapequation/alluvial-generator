23d0f7e3c46d8d66a5aecc7bfbef3e42d40d9981
Anton Eriksson <anton.eriksson@umu.se>
2018-09-26 14:09:16 +0200

    Cache rank and level.

--- src/models/Path.js
+++ src/models/Path.js
@@ -3,16 +3,20 @@ import TreePath from "../lib/treepath";
 
 export default class Path {
     +path: TreePath;
+    -_level: ?number = null;
+    -_rank: ?number = null;
 
     constructor(path) {
         this.path = new TreePath(path);
     }
 
     get rank() {
-        return this.path.rank;
+        if (!this._rank) this._rank = this.path.rank;
+        return this._rank;
     }
 
     get level() {
-        return this.path.level;
+        if (!this._level) this._level = this.path.level;
+        return this._level;
     }
 }
