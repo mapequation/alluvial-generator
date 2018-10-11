e2f14d5256bb41ffdd455761ef11f3a8098d374b
Anton Eriksson <anton.eriksson@umu.se>
2018-10-11 10:19:06 +0200

    Remove old layout algorithm

--- src/workers/actions.js
+++ src/workers/actions.js
@@ -1,3 +1,1 @@
-export const ACCUMULATE = "ACCUMULATE";
 export const ECHO = "ECHO";
-export const COORDINATES = "COORDINATES";
--- src/workers/worker.js
+++ src/workers/worker.js
@@ -1,6 +1,4 @@
-import { ACCUMULATE, COORDINATES, ECHO } from "./actions";
-import diagram from "../models/diagram";
-import accumulateModuleFlow from "../models/accumulate-module-flow";
+import { ECHO } from "./actions";
 
 
 const log = message => console.log(`[Worker] ${message}`);
@@ -17,13 +15,6 @@ onmessage = function onMessage(event) {
     log(`Got event ${type}`);
 
     switch (data.type) {
-        case ACCUMULATE:
-            const { sourceNodes, targetNodes } = data;
-            postMessage(accumulateModuleFlow(sourceNodes, targetNodes));
-            break;
-        case COORDINATES:
-            postMessage(diagram(data.props));
-            break;
         case ECHO:
             const { type, ...rest } = data;
             console.log(rest);
