ebef34a2d4fde38ed22cec6e50e8d2a8d709868d
Anton Eriksson <anton.eriksson@umu.se>
2018-10-23 23:45:23 +0200

    Disable flow in streamline.js

--- src/lib/streamline.js
+++ src/lib/streamline.js
@@ -1,17 +1,7 @@
-// @flow
 import { path } from "d3";
 
-type Points = {
-  x0: number,
-  y0: number,
-  h0: number,
-  x1: number,
-  y1: number,
-  h1: number
-};
-
 export function streamlineHorizontal() {
-  return function streamline({ x0, y0, h0, x1, y1, h1 }: Points): string {
+  return function streamline({ x0, y0, h0, x1, y1, h1 }) {
     const p0 = [x0, y0 + h0];
     const p1 = [x1, y1 + h1];
     const p2 = [x1, y1];
