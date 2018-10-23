1f1b175c4b637373caed428556ad824ab9c211a8
Anton Eriksson <anton.eriksson@umu.se>
2018-10-23 20:29:08 +0200

    Make LinearGradients a pure component

--- src/components/LinearGradients.js
+++ src/components/LinearGradients.js
@@ -2,7 +2,7 @@ import * as d3 from "d3";
 import PropTypes from "prop-types";
 import React from "react";
 
-export default class LinearGradients extends React.Component {
+export default class LinearGradients extends React.PureComponent {
   static defaultProps = {
     defaultColor: "white",
     highlightColors: []
@@ -13,44 +13,26 @@ export default class LinearGradients extends React.Component {
     highlightColors: PropTypes.arrayOf(PropTypes.string)
   };
 
-  componentDidMount() {
-    this.update();
-  }
-
-  componentDidUpdate() {
-    this.update();
-  }
-
-  update() {
-    const { defaultColor, highlightColors } = this.props;
-
-    const defs = d3.select(this.defs);
-    const highlightIndices = [-1, ...highlightColors.keys()];
-    const color = index =>
-      index === -1 ? defaultColor : highlightColors[index];
-
-    const gradients = defs
-      .selectAll("linearGradient")
-      .data(d3.cross(highlightIndices, highlightIndices));
-
-    gradients.exit().remove();
-
-    gradients
-      .enter()
-      .append("linearGradient")
-      .attr("id", d => `gradient_${d[0]}_${d[1]}`)
-      .selectAll("stop")
-      .data(d => [
-        { offset: "5%", color: color(d[0]) },
-        { offset: "95%", color: color(d[1]) }
-      ])
-      .enter()
-      .append("stop")
-      .attr("offset", d => d.offset)
-      .attr("stop-color", d => d.color);
-  }
+  static getUrl = (from, to) => `url(#gradient_${from}_${to})`;
 
   render() {
-    return <defs ref={node => (this.defs = node)} />;
+    const { defaultColor, highlightColors } = this.props;
+
+    const highlightIndices = [-1, ...highlightColors.keys()];
+    const pairs = d3.cross(highlightIndices, highlightIndices);
+    const color = index =>
+      index === -1 ? defaultColor : highlightColors[index];
+    const id = pair => `gradient_${pair[0]}_${pair[1]}`;
+
+    return (
+      <defs>
+        {pairs.map((pair, key) => (
+          <linearGradient key={key} id={id(pair)}>
+            <stop offset="5%" stopColor={color(pair[0])} />
+            <stop offset="95%" stopColor={color(pair[1])} />
+          </linearGradient>
+        ))}
+      </defs>
+    );
   }
 }
