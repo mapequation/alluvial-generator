e4eb69dcdb761ae2cec62d4ecf7e3b5828309123
Anton Eriksson <anton.eriksson@umu.se>
2019-05-05 20:48:40 +0200

    Update header font to Philosopher

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => (
           <h1 className="ui header">
             <a href="http://www.mapequation.org">
               <img className="mapequation-logo"
-                   src="http://www.mapequation.org/assets/img/twocolormapicon_whiteboarder.svg" alt="mapequation-icon"/>
+                   src="//www.mapequation.org/assets/img/twocolormapicon_whiteboarder.svg" alt="mapequation-icon"/>
             </a>
             <div className="content">
               <span className="brand">
