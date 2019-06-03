cd1dba8279c3c00df91313a794041bfdf5236176
Anton Eriksson <anton.eriksson@umu.se>
2019-06-03 11:31:59 +0200

    Inline StartPage in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
+import Documentation from "./Documentation";
+import Header from "./Header";
+import LoadNetworks from "./LoadNetworks";
 import Sidebar from "./Sidebar";
-import StartPage from "./StartPage";
 
 
 export default function App() {
   const [networks, setNetworks] = useState([]);
 
   if (networks.length === 0) {
-    return <StartPage onSubmit={setNetworks}/>;
+    return <React.Fragment>
+      <Header/>
+      <LoadNetworks onSubmit={setNetworks}/>
+      <Documentation/>
+    </React.Fragment>;
   }
 
   return <Sidebar networks={networks}/>;
