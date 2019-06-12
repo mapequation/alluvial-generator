a5735813df1c81d01365e41b2e84e5189f16b748
Anton Eriksson <anton.eriksson@umu.se>
2019-06-12 19:31:59 +0200

    Make DropShadows and LinearGradients functional

--- src/components/DropShadows.js
+++ src/components/DropShadows.js
@@ -1,49 +1,47 @@
-import * as d3 from "d3";
+import { range } from "d3";
 import PropTypes from "prop-types";
 import React from "react";
 
 
-export default class DropShadows extends React.PureComponent {
-  static defaultProps = {
-    maxLevel: 1
-  };
+export default function DropShadows(props) {
+  const { maxLevel } = props;
 
-  static propTypes = {
-    maxLevel: PropTypes.number
-  };
+  const levels = range(1, maxLevel + 1);
+  const x = level => maxLevel + 1 - level;
+  const id = level => `shadow${level}`;
 
-  static getUrl = level => `url(#shadow${level})`;
-
-  static filter = (enabled = true) => d =>
-    d.style("filter", enabled ? d => DropShadows.getUrl(d.moduleLevel) : null);
-
-  render() {
-    const { maxLevel } = this.props;
-
-    const levels = d3.range(1, maxLevel + 1);
-    const x = level => maxLevel + 1 - level;
-    const id = level => `shadow${level}`;
-
-    return (
-      <React.Fragment>
-        {levels.map((level, key) => (
-          <filter
-            key={key}
-            id={id(level)}
-            x="-50%"
-            y="-100%"
-            width="200%"
-            height="400%"
-          >
-            <feDropShadow
-              dx={0.5 * x(level)}
-              dy={0.5 * x(level)}
-              stdDeviation={0.5 * x(level)}
-              floodOpacity={-0.05 * x(level) + 0.95}
-            />
-          </filter>
-        ))}
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      {levels.map((level, key) => (
+        <filter
+          key={key}
+          id={id(level)}
+          x="-50%"
+          y="-100%"
+          width="200%"
+          height="400%"
+        >
+          <feDropShadow
+            dx={0.5 * x(level)}
+            dy={0.5 * x(level)}
+            stdDeviation={0.5 * x(level)}
+            floodOpacity={-0.05 * x(level) + 0.95}
+          />
+        </filter>
+      ))}
+    </React.Fragment>
+  );
 }
+
+DropShadows.defaultProps = {
+  maxLevel: 1
+};
+
+DropShadows.propTypes = {
+  maxLevel: PropTypes.number
+};
+
+DropShadows.getUrl = level => `url(#shadow${level})`;
+
+DropShadows.filter = (enabled = true) => d =>
+  d.style("filter", enabled ? d => DropShadows.getUrl(d.moduleLevel) : null);
--- src/components/LinearGradients.js
+++ src/components/LinearGradients.js
@@ -1,66 +1,63 @@
-import * as d3 from "d3";
+import { cross, hsl as d3_hsl } from "d3";
 import PropTypes from "prop-types";
 import React from "react";
 
 
-export default class LinearGradients extends React.PureComponent {
-  static defaultProps = {
-    defaultColor: "white",
-    highlightColors: []
+export default function LinearGradients(props) {
+  const { defaultColor, highlightColors } = props;
+
+  const highlightIndices = [-1, ...highlightColors.keys()];
+  const pairs = cross(highlightIndices, highlightIndices);
+  const color = index => index === -1 ? defaultColor : highlightColors[index];
+  const id = (left, right) => `gradient_${left}_${right}`;
+  const stroke = color => {
+    const hsl = d3_hsl(color);
+    hsl.s += 0.2;
+    hsl.l -= 0.2;
+    return hsl.toString();
   };
+  const strokeId = (left, right) => `gradient-stroke_${left}_${right}`;
 
-  static propTypes = {
-    defaultColor: PropTypes.string,
-    highlightColors: PropTypes.arrayOf(PropTypes.string)
-  };
+  const leftOffset = "15%";
+  const rightOffset = "85%";
 
-  static fill = d =>
-    d.attr(
-      "fill",
-      d => `url(#gradient_${d.leftHighlightIndex}_${d.rightHighlightIndex})`
-    );
-
-  static stroke = d =>
-    d.attr(
-      "stroke",
-      d =>
-        `url(#gradient-stroke_${d.leftHighlightIndex}_${d.rightHighlightIndex})`
-    );
-
-  render() {
-    const { defaultColor, highlightColors } = this.props;
-
-    const highlightIndices = [-1, ...highlightColors.keys()];
-    const pairs = d3.cross(highlightIndices, highlightIndices);
-    const color = index =>
-      index === -1 ? defaultColor : highlightColors[index];
-    const id = (left, right) => `gradient_${left}_${right}`;
-    const stroke = color => {
-      const hsl = d3.hsl(color);
-      hsl.s += 0.2;
-      hsl.l -= 0.2;
-      return hsl.toString();
-    };
-    const strokeId = (left, right) => `gradient-stroke_${left}_${right}`;
-
-    const leftOffset = "15%";
-    const rightOffset = "85%";
-
-    return (
-      <React.Fragment>
-        {pairs.map(([left, right], key) => (
-          <React.Fragment key={key}>
-            <linearGradient id={id(left, right)}>
-              <stop offset={leftOffset} stopColor={color(left)}/>
-              <stop offset={rightOffset} stopColor={color(right)}/>
-            </linearGradient>
-            <linearGradient id={strokeId(left, right)}>
-              <stop offset={leftOffset} stopColor={stroke(color(left))}/>
-              <stop offset={rightOffset} stopColor={stroke(color(right))}/>
-            </linearGradient>
-          </React.Fragment>
-        ))}
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      {pairs.map(([left, right], key) => (
+        <React.Fragment key={key}>
+          <linearGradient id={id(left, right)}>
+            <stop offset={leftOffset} stopColor={color(left)}/>
+            <stop offset={rightOffset} stopColor={color(right)}/>
+          </linearGradient>
+          <linearGradient id={strokeId(left, right)}>
+            <stop offset={leftOffset} stopColor={stroke(color(left))}/>
+            <stop offset={rightOffset} stopColor={stroke(color(right))}/>
+          </linearGradient>
+        </React.Fragment>
+      ))}
+    </React.Fragment>
+  );
 }
+
+LinearGradients.defaultProps = {
+  defaultColor: "white",
+  highlightColors: []
+};
+
+LinearGradients.propTypes = {
+  defaultColor: PropTypes.string,
+  highlightColors: PropTypes.arrayOf(PropTypes.string)
+};
+
+LinearGradients.fill = d =>
+  d.attr(
+    "fill",
+    d => `url(#gradient_${d.leftHighlightIndex}_${d.rightHighlightIndex})`
+  );
+
+LinearGradients.stroke = d =>
+  d.attr(
+    "stroke",
+    d =>
+      `url(#gradient-stroke_${d.leftHighlightIndex}_${d.rightHighlightIndex})`
+  );
