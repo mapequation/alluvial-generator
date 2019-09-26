3cd1b0703467edff02eb3eb59498dd6ec4d38277
Anton Eriksson <anton.eriksson@umu.se>
2019-09-26 17:32:31 +0200

    Enable Sentry report dialog

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { hydrate, render } from "react-dom";
-import * as Sentry from '@sentry/browser';
+import * as Sentry from "@sentry/browser";
 import App from "./components/App";
 import "./index.css";
 import { unregister } from "./registerServiceWorker";
 
-Sentry.init({dsn: "https://e37a7f0a2c724f1f826161eda4da92d1@sentry.io/1762779"});
+
+Sentry.init({
+  dsn: "https://e37a7f0a2c724f1f826161eda4da92d1@sentry.io/1762779",
+  beforeSend(event) {
+    // Check if it is an exception, and if so, show the report dialog
+    if (event.exception) {
+      Sentry.showReportDialog({ eventId: event.event_id });
+    }
+    return event;
+  }
+});
 
 const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
