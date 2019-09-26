2e080ffddd074264f0ba0c4bbd9b21352e9d1f93
Anton Eriksson <anton.eriksson@umu.se>
2019-09-26 17:15:18 +0200

    Add Sentry

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { hydrate, render } from "react-dom";
+import * as Sentry from '@sentry/browser';
 import App from "./components/App";
 import "./index.css";
 import { unregister } from "./registerServiceWorker";
 
+Sentry.init({dsn: "https://e37a7f0a2c724f1f826161eda4da92d1@sentry.io/1762779"});
 
 const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
