a53c16d8c9f33a5f60452ebba8d5a9329596c839
Anton Eriksson <anton.eriksson@umu.se>
2019-12-18 16:35:59 +0100

    perf: Remove ternary expression from opposite(side)

--- src/alluvial/Side.js
+++ src/alluvial/Side.js
@@ -3,11 +3,6 @@ export type Side = -1 | 1;
 export const LEFT: Side = -1;
 export const RIGHT: Side = 1;
 
-export default {
-  LEFT,
-  RIGHT
-};
-
 export const sideToString = (side: Side): string => side === LEFT ? "left" : "right";
 
-export const opposite = (side: Side): Side => (side === LEFT ? RIGHT : LEFT);
+export const opposite = (side: Side): Side => -side;
