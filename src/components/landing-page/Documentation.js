0f3b078b023d9fef18ad681b33912c34af505597
Anton Eriksson <anton.eriksson@umu.se>
2020-01-29 17:15:24 +0100

    docs: Improve documentation and add node comparison figure

--- src/components/landing-page/Documentation.js
+++ src/components/landing-page/Documentation.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Container, Grid, Header, Icon } from "semantic-ui-react";
+import { Container, Grid, Header, Icon, List } from "semantic-ui-react";
+import compareSvg from "../../images/compare.svg";
 import expandModuleMov from "../../images/expand.mov";
 import regroupModuleMov from "../../images/regroup.mov";
 import Changelog from "./Changelog";
 
 
-const videoProps = { autoPlay: true, loop: true, className: "ui", width: 400, height: 300 };
+const videoProps = { autoPlay: true, loop: true, className: "ui", width: 300, height: 225 };
 
 const Documentation = () =>
   <Container style={{ padding: "40px 0 100px 0" }}>
@@ -13,8 +14,11 @@ const Documentation = () =>
       <Grid.Column>
         <Header as="h1">Instructions</Header>
         <p>
-          Networks are shown as vertical stacks of modules. To highlight change between networks, we draw streamlines
-          between the networks. Sub-modules that have the same parent module are drawn closer together than other
+          Networks are shown as vertical stacks of modules. To highlight change between networks, we connect modules
+          with streamlines.
+        </p>
+        <p>
+          Sub-modules that have the same parent module are drawn closer together than other
           modules in the same network.
         </p>
 
@@ -26,6 +30,11 @@ const Documentation = () =>
         <p>
           If a network <i>does not have unique names</i>, you can try the option "Use node ids as identifiers",
           which uses the node ids to determine if two nodes are equal.
+        </p>
+        <img src={compareSvg} alt="Comparing nodes across networks" width={500}/>
+        <p>
+          Currently, we cannot compare state networks, except for the simple case when it is just the same network with
+          different Infomap options. This feature is under development!
         </p>
 
         <Header>Navigation</Header>
@@ -59,6 +68,32 @@ const Documentation = () =>
           All modules with the same parent will be re-grouped into the parent module.
         </p>
 
+        <Header as="h1">Supported formats</Header>
+        <p>
+          Currently, we support networks clustered by <a
+          href="https://www.mapequation.org/code.html">Infomap</a> v1. Older versions might work but is not supported.
+        </p>
+        <p>
+          The following output formats are supported:
+        </p>
+        <List bulleted>
+          <List.Item><code>clu</code></List.Item>
+          <List.Item><code>map</code></List.Item>
+          <List.Item><code>tree</code></List.Item>
+          <List.Item><code>ftree</code></List.Item>
+        </List>
+        <p>
+          To get hierarchically nested modules, you need to use the formats <code>tree</code> or <code>ftree</code>.
+        </p>
+        <p>
+          Infomap is available as a <a href="https://www.mapequation.org/code.html">stand-alone</a> C++ application
+          and on the web as <a href="https://www.mapequation.org/infomap">Infomap Online</a>.
+        </p>
+        <p>
+          Read more about Infomap output formats on the <a href="https://www.mapequation.org/code.html#Output">Infomap
+          documentation page</a>.
+        </p>
+
         <Header as="h1">Feedback</Header>
         <p>
           If you have any questions, suggestions or issues regarding the software, please add them to <a
@@ -75,21 +110,6 @@ const Documentation = () =>
         <p>
           D. Edler, A. Eriksson and M. Rosvall, The MapEquation software package,
           available online at <a href="https://www.mapequation.org">mapequation.org</a>.
-        </p>
-
-        <Header as="h1">Supported formats</Header>
-        <p>
-          Currently, we support networks clustered by <a href="https://www.mapequation.org/code.html">Infomap</a> into
-          the <code>clu</code>, <code>map</code>, <code>tree</code> and <code>ftree</code> formats.
-          To get hierarchically nested modules, you need to use the formats <code>tree</code> or <code>ftree</code>.
-        </p>
-        <p>
-          Infomap is available as a <a href="https://www.mapequation.org/code.html">stand-alone</a> C++ application
-          and on the web as <a href="https://www.mapequation.org/infomap">Infomap Online</a>.
-        </p>
-        <p>
-          Read more about Infomap output formats on the <a href="https://www.mapequation.org/code.html#Output">Infomap
-          documentation page</a>.
         </p>
       </Grid.Column>
       <Grid.Column>
