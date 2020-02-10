4a55e54e9e6c4e574bc7601da4f32275791fe2ed
Anton Eriksson <anton.eriksson@umu.se>
2020-02-10 13:34:35 +0100

    build: Disable Sentry on development

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ import "./index.css";
 import { unregister } from "./registerServiceWorker";
 
 
+const dsn = process.env.NODE_ENV === "production" ? "https://e37a7f0a2c724f1f826161eda4da92d1@sentry.io/1762779" : "";
+
 Sentry.init({
-  dsn: "https://e37a7f0a2c724f1f826161eda4da92d1@sentry.io/1762779",
+  dsn,
   release: "alluvial@" + process.env.REACT_APP_VERSION,
   beforeSend(event) {
     // Check if it is an exception, and if so, show the report dialog
