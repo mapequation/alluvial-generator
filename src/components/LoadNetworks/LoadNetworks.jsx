890c065ed90c5b78604e411204d72451db7ada03
Anton Eriksson <anton.eriksson@umu.se>
2022-01-22 08:55:09 +0100

    fix: Adaptive margin in FileBackground for large number of modules

--- src/components/LoadNetworks/LoadNetworks.jsx
+++ src/components/LoadNetworks/LoadNetworks.jsx
@@ -290,29 +290,33 @@ function FileBackground({ file, fill, ...props }) {
   );
 
   const height = 300;
-  const margin = 10;
-  const usableHeight =
-    values.length > 2 ? height - margin * (values.length - 1) : height;
+
+  const margin = values.length < 10 ? 10 : 100 / values.length;
+
+  const usableHeight = Math.max(
+    0,
+    values.length < 2 ? height : height - margin * (values.length - 1)
+  );
 
   let prevY = 0;
 
   return (
-    <svg width="100%" height="100%" opacity={0.2} {...props}>
-      {values.map((k, i) => {
-        const y = prevY;
-        const rectHeight = k * usableHeight;
-        prevY += rectHeight + margin;
-        return (
-          <rect
-            key={i}
-            x={0}
-            y={y}
-            width="100%"
-            height={rectHeight}
-            fill={fill}
-          />
-        );
-      })}
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 150 300"
+      opacity={0.2}
+      preserveAspectRatio="none"
+      {...props}
+    >
+      <g fill={fill}>
+        {values.map((flow, i) => {
+          const y = prevY;
+          const height = flow * usableHeight;
+          prevY += height + margin;
+          return <rect key={i} x={0} y={y} width="100%" height={height} />;
+        })}
+      </g>
     </svg>
   );
 }
