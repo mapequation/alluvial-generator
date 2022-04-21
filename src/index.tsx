28abc8dc78aa26b4024cd182261c2ad786bb8fc3
Anton Eriksson <anton.eriksson@umu.se>
2022-04-21 09:10:55 +0200

    chore: Cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import { theme } from "./theme";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+import { theme } from "./theme";
 
 ReactDOM.render(
   <StrictMode>
@@ -15,8 +14,3 @@ ReactDOM.render(
   </StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
