932c0edfe1bc10d932f20fc70bb28eeb98c3074c
Anton Eriksson <anton.eriksson@umu.se>
2022-04-29 11:45:27 +0200

    revert: "Add DiagramContext"
    This reverts commit d7f2a5a65f71dc79f38b2ded08a968ac07c83e8f.

--- src/components/Diagram/Module.tsx
+++ src/components/Diagram/Module.tsx
@@ -2,10 +2,9 @@ import { LightMode, Tooltip } from "@chakra-ui/react";
 import { motion, useAnimation } from "framer-motion";
 import { observer } from "mobx-react";
 import { forwardRef, useContext } from "react";
-import type { Module as ModuleType } from "../../alluvial";
+import type { HighlightGroup, Module as ModuleType } from "../../alluvial";
 import useOnClick from "../../hooks/useOnClick";
 import { StoreContext } from "../../store";
-import { DiagramContext } from "./Diagram";
 import DropShadows from "./DropShadows";
 import ModuleTooltip from "./ModuleTooltip";
 
@@ -13,9 +12,14 @@ const G = forwardRef<SVGGElement, any>(function Group(props, ref) {
   return <g ref={ref} {...props} />;
 });
 
-const Module = observer(function Module({ module }: { module: ModuleType }) {
+const Module = observer(function Module({
+  module,
+  fillColor,
+}: {
+  module: ModuleType;
+  fillColor: (_: HighlightGroup) => string;
+}) {
   const store = useContext(StoreContext);
-  const { fillColor } = useContext(DiagramContext);
   const controls = useAnimation();
   const { fontSize, adaptiveFontSize, showModuleId, showModuleNames } = store;
   const dropShadow = DropShadows.filter(store.dropShadow);
--- src/components/Diagram/Network.tsx
+++ src/components/Diagram/Network.tsx
@@ -2,6 +2,7 @@ import { motion, MotionProps } from "framer-motion";
 import { observer } from "mobx-react";
 import { SVGProps, useContext } from "react";
 import type {
+  HighlightGroup,
   Module as ModuleType,
   Network as NetworkType,
 } from "../../alluvial";
@@ -14,8 +15,10 @@ import Streamline from "./Streamline";
 
 const Network = observer(function Network({
   network,
+  fillColor,
 }: {
   network: NetworkType;
+  fillColor: (_: HighlightGroup) => string;
 }) {
   const {
     defaultHighlightColor,
@@ -64,7 +67,7 @@ const Network = observer(function Network({
       {modules.map((module) => {
         if (hierarchicalModules === "none" || !("isLeaf" in module)) {
           const m = module as ModuleType;
-          return <Module key={m.id} module={m} />;
+          return <Module key={m.id} module={m} fillColor={fillColor} />;
         } else if (hierarchicalModules === "shadow") {
           return (
             <ShadowModule
