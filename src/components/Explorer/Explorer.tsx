e2d91158214654bfb60e0a6b5ffcc4a4c0ed17d2
Anton Eriksson <anton.eriksson@umu.se>
2022-06-14 14:42:11 +0200

    feat: Download list of nodes in sub-modules

--- src/components/Explorer/Explorer.tsx
+++ src/components/Explorer/Explorer.tsx
@@ -14,10 +14,13 @@ import {
   Thead,
   Tr,
   useColorModeValue,
+  useToast,
 } from "@chakra-ui/react";
+import FileSaver from "file-saver";
 import { observer } from "mobx-react";
 import { useContext } from "react";
 import { StoreContext } from "../../store";
+import highlightColor from "../../utils/highlight-color";
 import { Swatch } from "../Sidebar/components";
 
 export default observer(function Explorer({
@@ -25,10 +28,12 @@ export default observer(function Explorer({
 }: {
   onClose: () => void;
 }) {
+  const toast = useToast();
   const store = useContext(StoreContext);
   const bg = useColorModeValue("white", "gray.700");
 
   const { selectedModule, defaultHighlightColor, highlightColors } = store;
+  const fillColor = highlightColor(defaultHighlightColor, highlightColors);
 
   if (selectedModule == null) {
     onClose();
@@ -36,6 +41,24 @@ export default observer(function Explorer({
   }
 
   const nodes = selectedModule.getLeafNodes().sort((a, b) => b.flow - a.flow);
+
+  const downloadNames = () => {
+    const names = nodes.map((node) => node.name).join("\n");
+    FileSaver.saveAs(
+      new Blob([names], { type: "text/plain;charset=utf-8" }),
+      `${selectedModule.networkName}-module-${selectedModule.moduleId}.txt`
+    );
+  };
+
+  const copyNames = async () => {
+    if (!navigator.clipboard) return;
+    const names = nodes.map((node) => node.name).join("\n");
+    await navigator.clipboard.writeText(names);
+    toast({
+      status: "success",
+      description: "Names copied to clipboard",
+    });
+  };
 
   return (
     <>
@@ -48,33 +71,27 @@ export default observer(function Explorer({
             <Table variant="striped" size="sm" colorScheme="gray">
               <Thead position="sticky" top={0} bg={bg}>
                 <Tr>
-                  <Th isNumeric>Id</Th>
+                  <Th>Color</Th>
+                  <Th>Name</Th>
                   <Th>Path</Th>
+                  <Th isNumeric>Id</Th>
                   {nodes.length > 0 && nodes[0].stateId != null && (
                     <Th isNumeric>State id</Th>
                   )}
                   <Th isNumeric>Flow</Th>
-                  <Th>Name</Th>
-                  <Th>Color</Th>
                 </Tr>
               </Thead>
               <Tbody>
                 {nodes.map((node) => (
                   <Tr key={node.nodeId}>
-                    <Td isNumeric>{node.nodeId}</Td>
+                    <Td>
+                      <Swatch color={fillColor(node)} />
+                    </Td>
+                    <Td>{node.name}</Td>
                     <Td>{node.treePath.toString()}</Td>
+                    <Td isNumeric>{node.nodeId}</Td>
                     {node.stateId != null && <Td isNumeric>{node.stateId}</Td>}
                     <Td isNumeric>{node.flow.toFixed(6)}</Td>
-                    <Td>{node.name}</Td>
-                    <Td>
-                      <Swatch
-                        color={
-                          node.highlightIndex === -1
-                            ? defaultHighlightColor
-                            : highlightColors[node.highlightIndex]
-                        }
-                      />
-                    </Td>
                   </Tr>
                 ))}
               </Tbody>
@@ -82,7 +99,17 @@ export default observer(function Explorer({
           </Box>
         </ModalBody>
         <ModalFooter>
-          <Button onClick={onClose}>Close</Button>
+          {navigator.clipboard != null && (
+            <Button mr="2" onClick={copyNames}>
+              Copy names to clipboard
+            </Button>
+          )}
+          <Button mr="auto" onClick={downloadNames}>
+            Download names
+          </Button>
+          <Button isActive onClick={onClose}>
+            Close
+          </Button>
         </ModalFooter>
       </ModalContent>
     </>
